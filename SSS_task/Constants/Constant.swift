1b3e55e51c1fbe412380ee8505fd7c709c3d80f5
Sunny Patel <sunnypatel@Mac-mini-2.local>
2023-10-19 11:16:07 +0530

    First commit, doesnt include any changes

---
+++ SSS_task/Constants/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  SSS_task
+//
+//  Created by Sunny Patel on 18/10/23.
+//
+
+enum Constant{
+    enum API{
+        static let plpAPI = "https://ov-dev.sssports.com/s/UAE/dw/shop/v20_10/product_search?client_id=ce6abb4e-faf1-41af-94e7-feb1e2dd4a77&count=24&expand=prices,%20images,%20represented_products&locale=en-AE&refine=cgid%3Dbrands_nike&refine_1=htype%3Dvariation_group%7Cproduct&refine_2=price%3D(1..)&refine_3=orderable_only%3Dtrue&start=0"
+    }
+}
--- SSS_task/Helper/APIManager.swift
+++ SSS_task/Helper/APIManager.swift
@@ -8,7 +8,40 @@
 import UIKit
 
 //Singleton Design Pattern
-class APIManager{
+
+enum DataError: Error {
+    case invalidResponse
+    case invalidURL
+    case invalidData
+    case network(Error?)
+}
+
+typealias Handler = (Result<ProductInformationModel, DataError>) -> Void
+
+final class APIManager{
     
     static let shared = APIManager()
+    private init() {}
+    
+    func fetchProducts(completion: @escaping Handler  ){
+        guard let url = URL(string: Constant.API.plpAPI) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(.failure(.invalidData))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, 200 ... 299 ~= response.statusCode else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            do {
+                let products = try JSONDecoder().decode(ProductInformationModel.self, from: data)
+                completion(.success(products))
+            } catch {
+                completion(.failure(.network(error)))
+            }
+        }.resume()
+        
+        
+    }
 }
